ab17335ffdca9156d211a9bebec1a88f3332fc10
Julien Ramboz <ramboz@adobe.com>
2022-11-07 20:17:41 +0100

    feat: use htmx

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,5 +1,3 @@
-import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
-
 /**
  * collapses all open nav sections
  * @param {Element} sections The container element
@@ -17,50 +15,39 @@ function collapseAllNavSections(sections) {
  */
 
 export default async function decorate(block) {
-  const cfg = readBlockConfig(block);
-  block.textContent = '';
+  // decorate nav DOM
+  const nav = document.createElement('nav');
+  nav.innerHTML = block.innerHTML;
 
-  // fetch nav content
-  const navPath = cfg.nav || '/nav';
-  const resp = await fetch(`${navPath}.plain.html`);
-  if (resp.ok) {
-    const html = await resp.text();
+  const classes = ['brand', 'sections', 'tools'];
+  classes.forEach((e, j) => {
+    const section = nav.children[j];
+    if (section) section.classList.add(`nav-${e}`);
+  });
 
-    // decorate nav DOM
-    const nav = document.createElement('nav');
-    nav.innerHTML = html;
-    decorateIcons(nav);
-
-    const classes = ['brand', 'sections', 'tools'];
-    classes.forEach((e, j) => {
-      const section = nav.children[j];
-      if (section) section.classList.add(`nav-${e}`);
-    });
-
-    const navSections = [...nav.children][1];
-    if (navSections) {
-      navSections.querySelectorAll(':scope > ul > li').forEach((navSection) => {
-        if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
-        navSection.addEventListener('click', () => {
-          const expanded = navSection.getAttribute('aria-expanded') === 'true';
-          collapseAllNavSections(navSections);
-          navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-        });
+  const navSections = [...nav.children][1];
+  if (navSections) {
+    navSections.querySelectorAll(':scope > ul > li').forEach((navSection) => {
+      if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
+      navSection.addEventListener('click', () => {
+        const expanded = navSection.getAttribute('aria-expanded') === 'true';
+        collapseAllNavSections(navSections);
+        navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
       });
-    }
-
-    // hamburger for mobile
-    const hamburger = document.createElement('div');
-    hamburger.classList.add('nav-hamburger');
-    hamburger.innerHTML = '<div class="nav-hamburger-icon"></div>';
-    hamburger.addEventListener('click', () => {
-      const expanded = nav.getAttribute('aria-expanded') === 'true';
-      document.body.style.overflowY = expanded ? '' : 'hidden';
-      nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
     });
-    nav.prepend(hamburger);
-    nav.setAttribute('aria-expanded', 'false');
-    decorateIcons(nav);
-    block.append(nav);
   }
+
+  // hamburger for mobile
+  const hamburger = document.createElement('div');
+  hamburger.classList.add('nav-hamburger');
+  hamburger.innerHTML = '<div class="nav-hamburger-icon"></div>';
+  hamburger.addEventListener('click', () => {
+    const expanded = nav.getAttribute('aria-expanded') === 'true';
+    document.body.style.overflowY = expanded ? '' : 'hidden';
+    nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+  });
+  nav.prepend(hamburger);
+  nav.setAttribute('aria-expanded', 'false');
+  block.innerHTML = '';
+  block.append(nav);
 }
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,124 +1,81 @@
 import {
-  sampleRUM,
-  buildBlock,
-  loadHeader,
-  loadFooter,
-  decorateButtons,
-  decorateIcons,
-  decorateSections,
-  decorateBlocks,
-  decorateTemplateAndTheme,
-  waitForLCP,
-  loadBlocks,
-  loadCSS,
+  loadBlock,
+  toCamelCase,
 } from './lib-franklin.js';
 
-const LCP_BLOCKS = []; // add your LCP blocks to the list
-window.hlx.RUM_GENERATION = 'project-1'; // add your RUM generation information here
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('main>div').forEach((section, i) => {
+    section.classList.add('section');
+    let defaultBlock;
+    [...section.children].forEach((block, j) => {
+      if (block.nodeName !== 'DIV' && !defaultBlock) {
+        defaultBlock = document.createElement('div');
+        if (!i && !j) {
+          defaultBlock.classList.add('hero');
+        } else {
+          defaultBlock.classList.add('default');
+        }
+        defaultBlock.classList.add('block');
+        [defaultBlock.dataset.blockName] = defaultBlock.classList;
+        section.insertBefore(defaultBlock, block);
+        defaultBlock.append(block);
+        return;
+      }
+      if (block.nodeName !== 'DIV') {
+        defaultBlock.append(block);
+        return;
+      }
 
-function buildHeroBlock(main) {
-  const h1 = main.querySelector('h1');
-  const picture = main.querySelector('picture');
-  // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
-    main.prepend(section);
+      if (defaultBlock) {
+        defaultBlock = null;
+      }
+
+      [block.dataset.blockName] = block.classList;
+      block.classList.add('block');
+
+      /* process section metadata */
+      if (block.dataset.blockName === 'section-metadata') {
+        [...block.children].forEach((child) => {
+          const prop = toCamelCase(child.children[0].innerText);
+          const value = child.children[1].innerText;
+          if (prop === 'style') {
+            section.classList.add(value);
+          } else {
+            section.dataset[prop] = value;
+          }
+        });
+        block.remove();
+      }
+    });
+  });
+
+  document.querySelectorAll('main a:only-child').forEach((button) => {
+    button.classList.add('button');
+    if (button.parentElement.nodeName === 'EM') {
+      button.classList.add('secondary');
+      button.parentElement.replaceWith(button);
+    }
+  });
+
+  document.querySelectorAll('body .block:not(.default):not(.htmx-request)').forEach(loadBlock);
+});
+
+const header = document.querySelector('body>header');
+header.classList.add('header');
+header.classList.add('block');
+header.dataset.blockName = 'header';
+header.dataset.hxGet = '/nav.plain.html';
+header.dataset.hxTrigger = 'load';
+
+const footer = document.querySelector('body>footer');
+footer.classList.add('footer');
+footer.classList.add('block');
+footer.dataset.blockName = 'footer';
+footer.dataset.hxGet = '/footer.plain.html';
+footer.dataset.hxTrigger = 'load';
+
+document.addEventListener('htmx:afterSettle', (ev) => {
+  if (ev.target.classList.contains('block')) {
+    loadBlock(ev.target);
   }
-}
-
-/**
- * Builds all synthetic blocks in a container element.
- * @param {Element} main The container element
- */
-function buildAutoBlocks(main) {
-  try {
-    buildHeroBlock(main);
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error('Auto Blocking failed', error);
-  }
-}
-
-/**
- * Decorates the main element.
- * @param {Element} main The main element
- */
-// eslint-disable-next-line import/prefer-default-export
-export function decorateMain(main) {
-  // hopefully forward compatible button decoration
-  decorateButtons(main);
-  decorateIcons(main);
-  buildAutoBlocks(main);
-  decorateSections(main);
-  decorateBlocks(main);
-}
-
-/**
- * loads everything needed to get to LCP.
- */
-async function loadEager(doc) {
-  document.documentElement.lang = 'en';
-  decorateTemplateAndTheme();
-  const main = doc.querySelector('main');
-  if (main) {
-    decorateMain(main);
-    await waitForLCP(LCP_BLOCKS);
-  }
-}
-
-/**
- * Adds the favicon.
- * @param {string} href The favicon URL
- */
-export function addFavIcon(href) {
-  const link = document.createElement('link');
-  link.rel = 'icon';
-  link.type = 'image/svg+xml';
-  link.href = href;
-  const existingLink = document.querySelector('head link[rel="icon"]');
-  if (existingLink) {
-    existingLink.parentElement.replaceChild(link, existingLink);
-  } else {
-    document.getElementsByTagName('head')[0].appendChild(link);
-  }
-}
-
-/**
- * loads everything that doesn't need to be delayed.
- */
-async function loadLazy(doc) {
-  const main = doc.querySelector('main');
-  await loadBlocks(main);
-
-  const { hash } = window.location;
-  const element = hash ? main.querySelector(hash) : false;
-  if (hash && element) element.scrollIntoView();
-
-  loadHeader(doc.querySelector('header'));
-  loadFooter(doc.querySelector('footer'));
-
-  loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
-  addFavIcon(`${window.hlx.codeBasePath}/styles/favicon.svg`);
-  sampleRUM('lazy');
-  sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
-  sampleRUM.observe(main.querySelectorAll('picture > img'));
-}
-
-/**
- * loads everything that happens a lot later, without impacting
- * the user experience.
- */
-function loadDelayed() {
-  // eslint-disable-next-line import/no-cycle
-  window.setTimeout(() => import('./delayed.js'), 3000);
-  // load anything that can be postponed to the latest here
-}
-
-async function loadPage() {
-  await loadEager(document);
-  await loadLazy(document);
-  loadDelayed();
-}
-
-loadPage();
+});
