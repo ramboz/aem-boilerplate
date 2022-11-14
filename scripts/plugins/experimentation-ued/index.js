849c4a765b71d6688acfd55400e079a7d4e92cb4
Julien Ramboz <ramboz@adobe.com>
2022-11-14 20:09:00 +0100

    Revert "Revert "fix: alloy loading""
    This reverts commit 6fbf3d4b793f2aac17f4ffbe1e3f524c825b9328.

--- scripts/plugins/experimentation-ued/index.js
+++ scripts/plugins/experimentation-ued/index.js
@@ -325,11 +325,6 @@ export async function preEager(customOptions, plugins) {
     ...customOptions,
   };
   await runExperiment(options, plugins);
-
-  !function(n,o){o.forEach(function(o){n[o]||((n.__alloyNS=n.__alloyNS||
-  []).push(o),n[o]=function(){var u=arguments;return new Promise(
-  function(i,l){n[o].q.push([i,l,u])})},n[o].q=[])})}
-  (window,["alloy"]);
 }
 
 export async function postEager() {
