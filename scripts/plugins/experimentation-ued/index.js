9c6c32aec99e8148ac2ea4668bb7bb5476b6aa4e
Julien Ramboz <ramboz@adobe.com>
2022-11-17 17:09:35 +0100

    refactor: revert minification

--- scripts/plugins/experimentation-ued/index.js
+++ scripts/plugins/experimentation-ued/index.js
@@ -326,9 +326,15 @@ export async function preEager(customOptions, plugins) {
     ...customOptions,
   };
   await runExperiment(options, plugins);
+
+  !function(n,o){o.forEach(function(o){n[o]||((n.__alloyNS=n.__alloyNS||
+  []).push(o),n[o]=function(){var u=arguments;return new Promise(
+  function(i,l){n[o].q.push([i,l,u])})},n[o].q=[])})}
+  (window,["alloy"]);
 }
 
-export async function postEager() {
+export async function preLazy() {
+  import('./alloy.js');
   if (window.location.hostname.endsWith('hlx.page') || window.location.hostname === ('localhost')) {
     // eslint-disable-next-line import/no-cycle
     import('./preview.js');
